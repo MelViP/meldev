017ee4adbb16540ab188a5d0a20cfa5da2209e36
MelPC <melanyvp98@gmail.com>
2023-10-08 21:35:47 -0600

    agregando animaciones de titulos y brillos

--- js/particles/gatoGithub.js
+++ js/particles/gatoGithub.js
@@ -1,15 +1,33 @@
-let canvas, paper, w, h, gato, orejas, ojos, cara;
+const canvas = document.querySelector(".gatitoPulpo");
+const contenedor = document.querySelector(".socialMedia > span")
 
-window.addEventListener("DOMContentLoaded",() => {
-	canvas = document.querySelector(".gatitoPulpo");
-	paper = canvas.getContext("2d");
-	resizeReset();
+const pincel = canvas.getContext('2d');
 
-    }
-);
+//canvas size
+canvas.width = contenedor;
+canvas.height = contenedor;
 
-//dibujar cabeza
-//dibujar orejas
-//dibujar ojos
-//dibujar cara
-window.addEventListener("resize", resizeReset);
+// Clear the canvas
+pincel.clearRect(0, 0, canvas.width, canvas.height);
+
+// Set the line width and color
+pincel.lineWidth = 5;
+pincel.strokeStyle = 'black';
+
+// Draw the GitHub icon
+pincel.beginPath();
+pincel.moveTo(20, 20);
+pincel.lineTo(80, 20);
+pincel.lineTo(80, 80);
+pincel.lineTo(20, 80);
+pincel.closePath();
+pincel.stroke();
+
+// Fill the GitHub icon with black
+pincel.fillStyle = 'black';
+pincel.fill();
+
+// Draw the GitHub logo
+pincel.fillStyle = 'white';
+pincel.font = 'bold 20px sans-serif';
+pincel.fillText('GitHub', 35, 50);
